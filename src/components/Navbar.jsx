ebb4b3c41d2ad85ffc5be767420ff41249274e2e
hanenedevfull <hanenedevfulljs@gmail.com>
2024-05-17 16:10:01 +0200

    create Banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo  from '/logo.png'
+import { BiPhoneCall } from "react-icons/bi";
 const Navbar = () => {
     const navItems= <>
      <li><a href="/">Home</a></li>
@@ -57,7 +58,16 @@ const Navbar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+  <button className="btn btn-ghost btn-circle hidden lg:flex">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+  <div tabIndex={0} role="button" className="btn btn-ghost btn-circle mr-3  hidden lg:flex items-center justify-center">
+        <div className="indicator">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+          <span className="badge badge-sm indicator-item">8</span>
+        </div></div>
+ 
+    <a className="btn bg-green rounded-full px-6 text-white flex items-center gap-2"><BiPhoneCall />Contact</a>
   </div>
 </div>
     </header>
