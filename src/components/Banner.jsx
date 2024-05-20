b4b811e616428b473a36e2bd8e78982d7c53e13d
hanenedevfull <hanenedevfulljs@gmail.com>
2024-05-20 19:34:51 +0200

    banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,76 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4'>Banner</div>
+    <div className='section-container  bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%'>
+      <div  className='py-24 flex flex-col md:flex-row-reverse justify-between items-center gap-8'>
+
+      <div className='md:w-1/2'>
+
+<img src="../../public/images/home/banner.png" alt="" />
+
+<div className='flex flex-col md:flex-row items-center justify-around -mt-14 gap-4'>
+
+<div className=' bg-white py-2 px-3 rounded-2xl  flex items-center gap-3 shadow-md w-64'>
+<img src="../../public/images/home/b-food1.png" alt=""  className='rounded-2xl'/>
+<div className='space-y-1'>
+
+  <h5 className='font-medium'>spicy noodles</h5>
+  <div className="rating rating-sm">
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" checked />
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
+</div>
+<p className='text-red'>$18.00</p>
+</div>
+
+
+</div>
+
+<div className='sm:flex hidden  bg-white py-2 px-3 rounded-2xl items-center gap-3 shadow-md w-64'>
+<img src="../../public/images/home/b-food1.png" alt=""  className='rounded-2xl'/>
+<div className='space-y-1'>
+
+  <h5 className='font-medium'>spicy noodles</h5>
+  <div className="rating rating-sm">
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" checked />
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500"  />
+<input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
+</div>
+<p className='text-red'>$18.00</p>
+</div>
+
+
+</div>
+
+
+
+</div>  
+    
+    
+    
+    </div>
+<div className='md:w-1/2 space-y-7 px-4'>
+  <h2 className='md:text-5xl text-4xl font-bold md:leading-snug leading-snug'>
+  Lorem ipsum dolor sit amet consectetur <sapn className='text-green'>adipisicing elit</sapn>
+  </h2>
+  <p className='text-xl text-[#4A4AAA]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aliquid, atque sequi quos saepe, molestiae dolorem quasi quisquam minus quis magni consequatur esse ducarchitecto aliquam temporibus obcaecati.</p>
+  <button className=' font-semibold text-white rounded-full btn bg-green px8 py-3'>Order Now</button>
+  </div>
+
+
+
+
+      
+  </div>
+
+
+
+
+  </div> 
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo  from '/logo.png'
 import { BiPhoneCall } from "react-icons/bi";
 const Navbar = () => {
+
+  const [isSicky, setIsSicky]=useState(false)
+  useEffect(()=>{
+    const handleScroll=()=>{
+      const offset=window.scrollY;
+      if(offset>0){
+        setIsSicky(true)
+      }else{
+        setIsSicky(false)
+      }
+    };
+    
+    
+    window.addEventListener("scroll",handleScroll);
+    return ()=>{
+      window.addEventListener("scroll",handleScroll);
+    }
+  },[])
+
+
+
     const navItems= <>
      <li><a href="/">Home</a></li>
       <li>
@@ -28,8 +49,10 @@ const Navbar = () => {
       </li>
       <li><a>Offres</a></li></>
   return (
-    <header className='max-w-screen-2xl container mx-auto'>
-        <div className="navbar xl:px-24">
+    <header className='max-w-screen-2xl 
+    container mx-auto fixed top-0 left-0 right-0
+    transition-all duration-300 easy-in-out'>
+        <div className={`navbar xl:px-24 ${isSicky ? "shadow-md bg-base-100 transition-all duration-300 easy-in-out" : " "}`}>
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
